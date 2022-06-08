2ebaade067526da6029da06d36e221ca0a865b89
Tim F <twforce@yahoo.com>
2022-06-08 16:48:24 -0700

    added temperatures

--- WeatherCL/weather_cl.py
+++ WeatherCL/weather_cl.py
@@ -3,6 +3,7 @@ import settings_mine
 import requests
 
 
+KELVIN_TEMP = 273.15
 print('Starting')
 
 api_key = settings_mine.api_key
@@ -15,8 +16,18 @@ url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + a
 request = requests.get(url)
 json = request.json()
 
+# parse data from json
 description = json.get('weather')[0].get('description')
-print("Today's forecase:", description)
+current_temp_celsius = json.get('main').get('temp') - KELVIN_TEMP
+min_temp_celsius = json.get('main').get('temp_min') - KELVIN_TEMP
+max_temp_celsius = json.get('main').get('temp_max') - KELVIN_TEMP
 
-print('Done.')
+# print("min_temp tyep: ", type(min_temp))
+
+print("Today's forecast: ", description)
+print("Current temperature: ", round(current_temp_celsius, 2), "C")
+print("Low: ", round(min_temp_celsius, 2), "C")
+print("High: ", round(max_temp_celsius, 2), "C")
+
+print('\nDone.')
 
