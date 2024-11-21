ee808c07152badfcf08ee546d96230fc945d9703
Tim Force <twforce@yahoo.com>
2024-11-20 16:26:52 -0800

    allow adding expenses

--- Expenses/expense_organizer.py
+++ Expenses/expense_organizer.py
@@ -7,10 +7,37 @@ class Expense:
         self.amount = amount
         self.date = date
 
+class ExpenseManager:
+    def __init__(self):
+        self.expenses = []
+    def add(self, expense):
+        self.expenses.append(expense)
+
+
+def add_expense(exp_manager):
+    print('***** add_expense() starting ... ')
+    amount = float(input('  amount: '))
+    desc = input('description: ')
+    date_str = input('date: ')
+    new_expense = Expense(desc, amount, date_str)
+    exp_manager.add(new_expense)
+
+
+def show_total_expenses():
+    print('***** show_total_expenses() starting ...')
+
+
+def view_expenses():
+    print('***** view_expenses() starting ...')
+
+
 
 def main():
-    print('main() staarting ...')
+    print('***** main() staarting ...')
     expense1 = Expense('food', 2.99, '2023-07-04')
+    expense_manager= ExpenseManager()
+
+    print('expense_manager: ', expense_manager.__dict__)
     print('expense1: ', expense1.__dict__)
 
     while True:
@@ -18,6 +45,14 @@ def main():
         print('2) total all expenses')
         print('3) view expenses')
         selection = input('Enter selection: ')
+        if selection == '1':
+            add_expense(expense_manager)
+        elif selection == '2':
+            show_total_expenses()
+        elif selection == '3':
+            view_expenses()
+        print('expenses now size: ', len(expense_manager.expenses))
+
 
 if __name__ == "__main__":
     main()
