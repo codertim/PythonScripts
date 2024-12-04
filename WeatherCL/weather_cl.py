2c9f267bf5a18883eeb81042d2b58f3c514eeb29
Tim Force <twforce@yahoo.com>
2024-12-03 16:28:38 -0800

    added main, added settings module file

--- WeatherCL/weather_cl.py
+++ WeatherCL/weather_cl.py
@@ -8,29 +8,38 @@ API_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 
 print('Starting ...\n')
+print('settings_mine:', settings_mine)
+print('type for settings_min:', type(settings_mine))
 
-api_key = settings_mine.api_key
-# city = 'Seattle'
-state = 'CA'
 
-city = input('Enter city: ') 
-url = API_BASE_URL + '?q=' + city + '&appid=' + api_key
+def main():
+    api_key = settings_mine.api_key
+    # city = 'Seattle'
+    state = 'CA'
 
-request = requests.get(url)
-json = request.json()
+    city = input('Enter city: ') 
+    url = API_BASE_URL + '?q=' + city + '&appid=' + api_key
 
-# parse data from json
-description = json.get('weather')[0].get('description')
-current_temp_celsius = json.get('main').get('temp') - KELVIN_OFFSET
-min_temp_celsius = json.get('main').get('temp_min') - KELVIN_OFFSET
-max_temp_celsius = json.get('main').get('temp_max') - KELVIN_OFFSET
+    request = requests.get(url)
+    json = request.json()
 
-# print("min_temp type: ", type(min_temp))
+    # parse data from json
+    description = json.get('weather')[0].get('description')
+    current_temp_celsius = json.get('main').get('temp') - KELVIN_OFFSET
+    min_temp_celsius = json.get('main').get('temp_min') - KELVIN_OFFSET
+    max_temp_celsius = json.get('main').get('temp_max') - KELVIN_OFFSET
 
-print("Today's forecast: ", description)
-print("Current temperature: ", round(current_temp_celsius, 2), "C")
-print("Low: ", round(min_temp_celsius, 2), "C")
-print("High: ", round(max_temp_celsius, 2), "C")
+    # print("min_temp type: ", type(min_temp))
+
+    print("Today's forecast: ", description)
+    print("Current temperature: ", round(current_temp_celsius, 2), "C")
+    print("Low: ", round(min_temp_celsius, 2), "C")
+    print("High: ", round(max_temp_celsius, 2), "C")
+
+
+if __name__ == "main":
+    main()
+
 
 print('\nDone.')
 
