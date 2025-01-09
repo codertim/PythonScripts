d38d1bd1b16cf49ffc1782c2af30cccd25bdcffa
Tim Force <twforce@yahoo.com>
2025-01-08 18:36:45 -0800

    adding new web scraper script to look at links quickly from a web site

---
+++ WebScraperLinks/web_scraper_links.py
@@ -0,0 +1,27 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+print('\nStarting ...')
+
+url = 'https://www.arstechnica.com'
+
+response = requests.get(url)
+
+print(f"Status Code: {response.status_code}")
+# print(response.text) 
+
+if response.status_code >= 200 and response.status_code < 300:
+    b_soup = BeautifulSoup(response.text, 'html.parser')
+    all_links = b_soup.find_all('a')
+    print(f"Total number of links: {len(all_links)}")
+    for idx, link in enumerate(all_links):
+        # href, text, title
+        print(f"{idx + 1}. {link.text}")
+        print(f"    href: {link.get('href')}")
+        print(f"    title: {link.get('title')}")
+else:
+    print('Error getting web page.')
+
+print('Done.')
+
